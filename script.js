cc7db35dc941e714bffe8e7dad8dca2a90889680
Alexej Holad <alexej.holad@gmail.com>
2020-10-30 09:42:45 +0100

    If else if Hello strangergit add -A!

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
+"use strict";
 
+// var cat = {
+//     name: "Harmony",
+//     legs: 3,
+//     color: "Leoparden",
+//     age: 8
+// };
+
+// var dog = {
+//     name: "Oladushek",
+//     color: "white",
+//     age: 8,
+//     bark: "Gav!"
+// };
+
+// console.log(Object.keys(dog));
+// console.log(Object.keys(cat));
+
+// let object = {};
+
+
+
+// var name = "Nico";
+
+// if (name.length > 6) {
+//     console.log("So long Name");
+// } else {
+//     console.log("Schöner Name");
+// };
+
+let name;
+
+if (name === 'Alex') {
+    console.log('Hello me!');
+} else if (name === 'Vasilij') {
+    console.log('Hello father!!!');
+} else if (name === 'Marina') {
+    console.log('Hello mother!!');
+} else {
+    console.log('Hello Stranger!!!');
+};
