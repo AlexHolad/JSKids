8c8f2785cf06c68e77826bf38e70ac87fda3b381
Alexej Holad <alexej.holad@gmail.com>
2020-11-02 09:04:27 +0100

    array for

--- script.js
+++ script.js
@@ -1,5 +1,100 @@
 "use strict";
 
+
+
+
+// let input = "javascript is awesome";
+// let output = "";
+
+
+// for (i = 0; i < input.length; i++) {
+//     if (input[i] === "a") {
+//         output += "4";
+//     } else if (input[i] == "e") {
+//         output += "3";
+//     } else if (input[i] == "i") {
+//         output += "1";;
+//     } else if (input[i] == "o") {
+//         output += "0";
+//     } else {
+//         output += input[i];
+//     }
+//     console.log(output);
+// };
+
+
+
+
+// var alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+// let randomString = "";
+// let randomIndex = Math.floor(Math.random() * alphabet.length);
+
+
+// while (randomString.length < 7) {
+//     randomIndex = Math.floor(Math.random() * alphabet.length);
+//     randomString += alphabet[randomIndex];
+
+// }
+
+// console.log(randomString);
+
+
+
+
+
+// var animals = ["Кот", "Рыба", "Лемур", "Комодский варан"];
+
+// for (i = 0; i < animals.length; i++) {
+//     animals[i] += " - прекрасное животное!";
+// };
+// console.log(animals);
+
+
+
+
+// for (let x = 3; x < 10000; x = x * 3) {
+//     console.log(x);
+// };
+
+// let x = 3;
+// while (x < 10000) {
+//     console.log(x);
+//     x *= 3;
+
+// };
+
+
+
+
+// let name = 'Nico';
+
+// for (i = 0; i < name.length; i++) {
+//     console.log('My name have letter: ' + name[i]);
+// };
+
+
+
+
+// let animals = ['leopard', 'elephant', 'monkey', 'bird', 'bear', 'flamingo'];
+
+// for (i = 0; i < animals.length; i++) {
+//     console.log('В этом зоопарке есть ' + animals[i] + '.');
+// };
+// console.log(animals.length);
+
+
+
+// let timeToSayHello = 3;
+// for (i = 0; i < timeToSayHello; i++ ) {
+//     console.log('Hello!');
+// };
+
+// for (i = 0; i < 10; i++) {
+//     console.log('Sheeps = ' + (i + 1));
+// };
+// console.log("Хрррррррррр-псссс");
+
+
 // var cat = {
 //     name: "Harmony",
 //     legs: 3,
@@ -29,14 +124,24 @@
 //     console.log("Schöner Name");
 // };
 
-let name;
+// let name;
 
-if (name === 'Alex') {
-    console.log('Hello me!');
-} else if (name === 'Vasilij') {
-    console.log('Hello father!!!');
-} else if (name === 'Marina') {
-    console.log('Hello mother!!');
-} else {
-    console.log('Hello Stranger!!!');
-};
+// if (name === 'Alex') {
+//     console.log('Hello me!');
+// } else if (name === 'Vasilij') {
+//     console.log('Hello father!!!');
+// } else if (name === 'Marina') {
+//     console.log('Hello mother!!');
+// } else {
+//     console.log('Hello Stranger!!!');
+// };
+
+// var sheepCounter = 0;
+
+// while (sheepCounter < 10) {
+//     console.log("Sheeps = " + (sheepCounter + 1));
+//     sheepCounter++;
+// };
+// console.log("Хрррррррррр-псссс");
+
+
